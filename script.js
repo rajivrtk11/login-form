ba20ad02547ab9b2c68a4655ddd77476e5fcfd0c
rajivrtk11 <rajivrtk11@gmail.com>
2021-09-03 17:12:41 +0530

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let fullName = document.querySelector(".full-name input");
+let email = document.querySelector(".email input");
+let password = document.querySelector(".password input");
+let confirmPass = document.querySelector(".confirm-pw input");
+let signUp = document.querySelector(".sign-up");
+
+signUp.addEventListener("onClick", function(){
+    
+})
